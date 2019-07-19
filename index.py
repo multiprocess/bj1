72033130ccf98bc07e15c55258cc117783edeaa0
multiprocess <389193007@qq.com>
2019-07-19 13:04:38 +0800

    index修改

--- index.py
+++ index.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+from djano.shortcuts import redirect
 
 def index(request):
     return HttpResponse('ddf')
+
+def redirect(request):
+    return redirect('/index/')
